c0b7d152ffcc2d4c4fb1d162f91e4f8512bc8651
chrxtina <christina.ad.nguyen@gmail.com>
2016-09-30 10:26:19 -0400

    change CLIENT_ORIGIN to localhost:8080

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module PaiselApi
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'https://chrxtina.github.io'
+        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
